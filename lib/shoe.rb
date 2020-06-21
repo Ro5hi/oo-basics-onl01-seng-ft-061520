2b537ebe1283055ee1d4df70c7bf3feaaa3a7e28
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-21 21:50:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,15 +5,9 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
- 
-  def brand=(shoe_brand)
-    @brand 
-  end 
-end 
 
 def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
