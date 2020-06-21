5a9efbd0a87dc7a8a3e0e23a2fc8bc688c001c94
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-21 21:53:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,15 @@
-class Shoe 
- attr_accessor :material :condition :color :size 
- attr_reader :brand
+# class Shoe 
+# attr_accessor :material :condition :color :size 
+# attr_reader :brand
  
-  def initialize(brand)
-      @brand = brand
-    if !(BRANDS.include? (@brand))
-      BRANDS << @brand
+#   def initialize(brand)
+#       @brand = brand
+#     if !(BRANDS.include? (@brand))
+#       BRANDS << @brand
     
-  end
+#   end
 
-# def cobble
-#     puts "Your shoe is as good as new!"
+# # def cobble
+# #     puts "Your shoe is as good as new!"
   
-# end
+# # end
